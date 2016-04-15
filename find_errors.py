855bcd9b1b8fe76c6337fb6602a2a790fb714a51
Debidatta Dwibedi <debi.me@gmail.com>
2016-04-15 12:15:32 -0400

    corrections for proper resuming of interrupted ief training

--- find_errors.py
+++ find_errors.py
@@ -16,18 +16,19 @@ def find_errors(prototxt, caffemodel, error_file, batch_count, batch_size, gpu_i
                     caffe.TEST)
     with open(error_file, 'w') as f:
         for i in range(batch_count):
+            print i
             net.forward()
             for j in range(batch_size):
                 errors = net.blobs['fc8_err_kps'].data[j,:]
                 true_labels = net.blobs['error_kps'].data[j,:]
                 for k in xrange(100):
                     f.write('%s '%errors[k])
-                    f.write("\n")
+                f.write("\n")
     
 if __name__ == "__main__":
     prototxt = sys.argv[1]
     caffemodel = sys.argv[2]
-    batch_count = 317#25
-    batch_size = 40
+    batch_count = int(sys.argv[4])
+    batch_size = int(sys.argv[5])
     error_file = sys.argv[3]
-    find_errors(prototxt, caffemodel, error_file, batch_count, batch_size)
+    find_errors(prototxt, caffemodel, error_file, batch_count, batch_size, 1)
--- ief_train.py
+++ ief_train.py
@@ -2,6 +2,7 @@ from __future__ import division
 import sys
 import argparse
 from shutil import copyfile
+import os
 
 caffe_root = '/home/debidatd/parsenet/'
 sys.path.insert(0, caffe_root + 'python')
@@ -21,11 +22,11 @@ parser.add_argument("vgg_net", help="Train prototxt path of network from where R
 parser.add_argument("vgg_weights", help="Caffemodel path of network from where RGB weights will be copied")
 parser.add_argument("caffemodel", help="Prefix of caffemodels will be stored after every run. This is independent of solver snapshotting.")
 parser.add_argument("--gpu", help="GPU ID", default=0, type=int)
-parser.add_argument("--resume", help="Flag to know if training is resuming or being done from scratch", action="store_true")
+parser.add_argument("--resume", help="Resume from which run if not being done from scratch", type=int, default=0)
 parser.add_argument("--train_iter", help="Number of train iterations per run", default=750, type=int)
 parser.add_argument("--test_iter", help="Number of test iterations", default=50, type=int)
 parser.add_argument("--test_interval", help="Test interval or run test after test_interval number of training iterations", default=50, type=int)
-parser.add_argument("--kps_folder", help="Folder to store keypoints after each run and error in keypoint", default='curr_kps/', type=int)
+parser.add_argument("--kps_folder", help="Folder to store keypoints after each run and error in keypoint", default='curr_kps/')
 args = parser.parse_args()
 
 # caffe.set_mode_cpu()
@@ -35,25 +36,26 @@ solver = caffe.SGDSolver(args.solver)
 solver.net.copy_from(args.weights)
 
 # We want to copy RGB weights from a pretrained network when we start training
-if not args.resume:
+if args.resume == 0:
     net_vgg = caffe.Net(args.vgg_net, args.vgg_weights, caffe.TEST) 
     solver.net.params['conv1d'][0].data[:,:3,:,:] = np.copy(net_vgg.params['conv1'][0].data[:,:3,:,:])
     solver.test_nets[0].params['conv1d'][0].data[:,:3,:,:] = np.copy(net_vgg.params['conv1'][0].data[:,:3,:,:])
     del net_vgg
-
+else:
+    last_run_snapshot = '%s_run%s.caffemodel'%(args.caffemodel, args.resume - 1)
 train_loss = np.zeros(args.train_iter)
 test_loss = np.zeros(args.train_iter)
 f = open(args.log_train, 'w')
 g = open(args.log_test, 'w')
 
-copyfile('curr_kps_train.txt', os.path.join(args.kps_folder, 'curr_kps_train_0.txt')
-copyfile('curr_kps_test.txt', os.path.join(args.kps_folder, 'curr_kps_test_0.txt')
-train_net = 'prototxts/error_train.txt'
-test_net = 'prototxts/error_test.txt'
+copyfile('curr_kps_train.txt', os.path.join(args.kps_folder, 'curr_kps_train_0.txt'))
+copyfile('curr_kps_test.txt', os.path.join(args.kps_folder, 'curr_kps_test_0.txt'))
+train_net = 'prototxts/error_train.prototxt'
+test_net = 'prototxts/error_test.prototxt'
 test_niter = 25
-train_iter = 317
+train_niter = 317
 
-for n in xrange(args.runs):
+for n in xrange(args.resume, args.runs):
     f.write("Begin run: %s\n"%n)
     g.write("Begin run: %s\n"%n)
     if n > 0:
@@ -65,29 +67,29 @@ for n in xrange(args.runs):
         f.write('{} {}\n'.format(i, np.sqrt(train_loss[i])))
         f.flush()
         if i%args.test_interval  == 0:
-            for j in xrange(test_iter):
+            for j in xrange(args.test_iter):
                 solver.test_nets[0].forward()
                 test_loss[i] += solver.test_nets[0].blobs['loss'].data
-            test_loss[i] = test_loss[i]/test_iter
+            test_loss[i] = test_loss[i]/args.test_iter
             g.write('{} {}\n'.format(i, np.sqrt(test_loss[i])))
             g.flush()
     f.write("End run: %s\n*********\n"%n)
     g.write("End run: %s\n*********\n"%n)
-    last_run_snapshot = '%s_run%s.caffemodel'%(args.caffemodels, n)
+    last_run_snapshot = '%s_run%s.caffemodel'%(args.caffemodel, n)
     solver.net.save(last_run_snapshot)
     del solver
    
     error_kps_file = os.path.join(args.kps_folder, 'error_train_%s.txt'%(n))
-    find_errors(train_net, last_run_snapshot, error_kps_file, train_niter, 40)
+    find_errors(train_net, last_run_snapshot, error_kps_file, train_niter, 40, args.gpu)
     curr_kps_train_file = os.path.join(args.kps_folder, 'curr_kps_train_%s.txt'%n)
     add_errors(curr_kps_train_file, error_kps_file, 'curr_kps_train.txt')
     copyfile('curr_kps_train.txt', os.path.join(args.kps_folder, 'curr_kps_train_%s.txt'%(n+1)))
 
     error_kps_file = os.path.join(args.kps_folder, 'error_test_%s.txt'%(n))
-    find_errors(test_net, last_run_snapshot, error_kps_file, test_niter, 40)
+    find_errors(test_net, last_run_snapshot, error_kps_file, test_niter, 40, args.gpu)
     curr_kps_test_file = os.path.join(args.kps_folder, 'curr_kps_test_%s.txt'%n)
     add_errors(curr_kps_test_file, error_kps_file, 'curr_kps_test.txt')
-    copyfile('curr_kps_test.txt', os.path.join(args.kps_folder, 'curr_kps_test_%s.txt'%(n+1))
+    copyfile('curr_kps_test.txt', os.path.join(args.kps_folder, 'curr_kps_test_%s.txt'%(n+1)))
 
 f.close()
 g.close()
